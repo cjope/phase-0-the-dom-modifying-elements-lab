a069d98b4e34da3e39c77a3c93fa010a80169827
cjope <craigjope@gmail.com>
2021-05-27 23:09:28 -0600

    Completed Assignment

--- index.js
+++ index.js
@@ -1,1 +1,6 @@
-// Write your code here!
+const main = document.getElementById('main');
+main.remove();
+const newHeader = document.createElement('h1');
+document.body.appendChild(newHeader);
+newHeader.innerHTML = "<h1>h1#victory</h1><p>CRAIG is the champion</p></h1>";
+newHeader.id = "victory";
